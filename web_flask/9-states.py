a230efb94b786d8dc552640971bfedc9eedf7ffc
Omowunmijuin <balikiskamil@gmail.com>
2023-07-27 02:55:37 +0000

    pycodestyle

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,6 +6,7 @@ Starting a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
 
 
@@ -13,9 +14,16 @@ app = Flask(__name__)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
     """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-    state_id = 'State.' + state_id
+    if id:
+        states = storage.all("State")
+        key = 'State.' + state_id
+        if key in states:
+            state = states[key]
+        else:
+            state = None
+        states = []
+    else:
+        states = list(storage.all(state).values())
     return render_template('9-states.html', states=states, state_id=state_id)
 
 
@@ -23,6 +31,7 @@ def states(state_id=None):
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
